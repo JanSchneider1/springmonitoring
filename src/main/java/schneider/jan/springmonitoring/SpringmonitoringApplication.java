a733e2a4941c95f74927c34a0de7770adba1c005
JanSchneider1 <jan.schneider@adesso.de>
2020-07-13 13:05:44 +0200

    Initial commit

---
+++ src/main/java/schneider/jan/springmonitoring/SpringmonitoringApplication.java
@@ -0,0 +1,12 @@
+package schneider.jan.springmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmonitoringApplication.class, args);
+    }
+}
---
+++ src/test/java/schneider/jan/springmonitoring/SpringmonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package schneider.jan.springmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
