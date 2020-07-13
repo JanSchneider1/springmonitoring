a8e910c8b391bcc39bd0c52b665c2833b000a350
JanSchneider1 <jan.schneider@adesso.de>
2020-07-13 13:34:43 +0200

    Added Rest-Controller and Spring Boot Actuator

--- src/main/java/schneider/jan/springmonitoring/Application.java
+++ src/main/java/schneider/jan/springmonitoring/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringmonitoringApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringmonitoringApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
