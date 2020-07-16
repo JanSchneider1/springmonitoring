3f6d9e41883fe93c2c028ee904b7919a818415c8
JanSchneider1 <jan.schneider@adesso.de>
2020-07-16 15:19:11 +0200

    Added counters

--- src/main/java/schneider/jan/springmonitoring/cats/CatService.java
+++ src/main/java/schneider/jan/springmonitoring/cats/CatService.java
@@ -1,8 +1,14 @@
 package schneider.jan.springmonitoring.cats;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.FunctionCounter;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Tags;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Array;
 import java.util.List;
 
 @Service
@@ -10,16 +16,24 @@ public class CatService {
 
     private final CatRepository catRepository;
 
+    private Counter catsAddedCounter = Metrics.counter("cats.added");
+    private Counter catsQueryCallsCounter = Metrics.counter("cats.query.calls");
+    private Counter catsQueriedCounter = Metrics.counter("cats.queried");
+
     @Autowired
     public CatService(CatRepository catRepository) {
         this.catRepository = catRepository;
     }
 
     public List<Cat> getAllCats() {
-        return catRepository.findAll();
+        catsQueryCallsCounter.increment();
+        List<Cat> cats = catRepository.findAll();
+        catsQueriedCounter.increment(cats.size());
+        return cats;
     }
 
     public Cat save(Cat cat) {
+        catsAddedCounter.increment();
         return catRepository.save(cat);
     }
 }
